92170bef0c53561d9d1d4eb5fb39f1448d4590ec
Satyanarayan <satyanarayan.y@gmail.com>
2023-12-12 12:05:54 +0530

    Quiz Services

---
+++ QuizService/src/test/java/com/srysoft/quiz/QuizServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.srysoft.quiz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuizServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
